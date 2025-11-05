914f09c24699ad35b02d6044d370f3aec1341bb2
Caden <cadenhgamer@gmail.com>
2025-11-04 20:44:29 -0700

    Finished up the Draw-O-Matic, experimented woth custom html pages for games

--- Draw-O-Matic/sketch.js
+++ Draw-O-Matic/sketch.js
@@ -8,12 +8,19 @@ let circleButtonClicked = false;
 let rectButtonClicked = true;
 let size;
 let currentColor;
+let opacity;
 
 function setup() {
   const circbtn = document.getElementById("circleButton");
   const rectbtn = document.getElementById("squareButton");
   const size = document.getElementById("sizeSlider");
   const sizeValue = document.getElementById("sizeValue");
+  const opacitySlider = document.getElementById("opacitySlider");
+  const opacityValue = document.getElementById("opacityValue");
+  opacitySlider.addEventListener("input", function () {
+    opacityValue.textContent = opacitySlider.value;
+  });
+  opacityValue.textContent = opacitySlider.value;
   size.addEventListener("input", function () {
     sizeValue.textContent = size.value;
   });
@@ -23,7 +30,7 @@ function setup() {
   rectbtn.addEventListener("click", makeitarect);
   let canvas = createCanvas(800, 600, defaultCanvas0);
   rectMode(CENTER);
-  background(220);
+  background(255);
   // size = createSlider(5, 50, 20);
   // size.position(300, 10);
   checkWindowSize();
@@ -52,11 +59,30 @@ function makeitarect() {
   circleButtonClicked = false;
 }
 
+//Had to use this function to convert hex to rgba for opacity control
+//Still can undertand most of it though :)
+function hexToRgbA(hex) {
+  var c;
+  if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+    c = hex.substring(1).split("");
+    if (c.length == 3) {
+      c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+    }
+    c = "0x" + c.join("");
+    return (
+      "rgba(" + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(",") + ",1)"
+    );
+  }
+  throw new Error("Bad Hex");
+}
+
 function mouseDragged() {
   console.log(colorPicker.value);
   const col = colorPicker.value;
   const brushSize = parseInt(sizeSlider.value);
-  fill(col);
+  const opacity = opacitySlider.value;
+  print(col);
+  fill(hexToRgbA(col).replace("1)", opacity / 100 + ")"));
   if (rectButtonClicked === true) {
     rect(mouseX, mouseY, brushSize, brushSize);
   }
@@ -68,7 +94,7 @@ function mouseDragged() {
 function keyPressed() {
   if (key === "c" || key === "C") {
     clear();
-    background(220);
+    background(255);
   }
   if (key === "s" || key === "S") {
     saveCanvas("myDrawing", "png");
